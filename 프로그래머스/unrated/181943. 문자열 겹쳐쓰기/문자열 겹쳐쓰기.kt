97439fb90ee62db9315ae0ce0e79fa11ac31c959
Kyungwon Seo <63994167+woojugoing@users.noreply.github.com>
2023-11-23 17:02:09 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 16.92 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -1,6 +1,5 @@
 class Solution {
-    fun solution(my_string: String, overwrite_string: String, s: Int): String {
-        return my_string.substring(0, s) + overwrite_string + 
+    fun solution(my_string: String, overwrite_string: String, s: Int): String =
+        my_string.substring(0, s) + overwrite_string + 
         my_string.substring(s + overwrite_string.length)
-    }
 }
